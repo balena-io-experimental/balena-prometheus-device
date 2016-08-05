8b00b3fa0963d151899c8be529830531d1cb65fc
Craig Mulligan <craig@resin.io>
2016-08-05 17:43:44 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 # Start the node exporter
-cd /etc/node_exporter-$NODE_EXPORTER_VERSION.$DIST_ARCH \
-  && ./node_exporter -web.listen-address ":80" &
 echo "Your application code should go here!"
+cd /etc/node_exporter-$NODE_EXPORTER_VERSION.$DIST_ARCH \
+  && ./node_exporter -web.listen-address ":80"
