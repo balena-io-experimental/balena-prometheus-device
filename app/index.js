bd798be780c6b550ba682ca8507bbf7f85eab3da
Tim Perry <pimterry@gmail.com>
2018-02-01 00:09:32 +0100

    Use the configured log colour

--- app/index.js
+++ app/index.js
@@ -4,13 +4,13 @@ leds.setup();
 
 const DEFAULT_COLOUR = [255, 0, 0];
 
+console.log('Flashing pixels');
 leds.setAllPixels(255, 255, 255, 1);
 leds.sendUpdate();
 
 setTimeout(() => {
-    console.log('Process.env.COLOUR', process.env.COLOUR);
     let colour = process.env.COLOUR ? JSON.parse(process.env.COLOUR) : DEFAULT_COLOUR;
-    console.log('colour', colour);
-    leds.setAllPixels(...DEFAULT_COLOUR, 0.5);
+    console.log('Setting colour to', colour);
+    leds.setAllPixels(...colour, 0.4);
     leds.sendUpdate();
 }, 1000);
